4aa743a526a1eb3d1dd6e6f81eb73ec442717730
vitek1 <wtsmolinski@gmail.com>
2021-01-23 18:19:17 -0500

    fixed folder path for datamgr in the assembly file, and for some reason the cdn endpoints didn't need https before and now they do

--- src/assembly.js
+++ src/assembly.js
@@ -2,9 +2,9 @@ requirejs.config({
     'baseUrl': 'src',
     'paths': {
         'main': 'main',
-        'jq': '//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min',
-        'jqui': '//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min',
-        'mgr': '../api/datamgr'
+        'jq': 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min',
+        'jqui': 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min',
+        'mgr': '../api/data/datamgr'
     }
 });
 
